6a16426dde8c137280ce65be02c690ef3d4f93b0
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 14:54:34 -0700

    woo

--- handlers/summary.rb
+++ handlers/summary.rb
@@ -3,8 +3,14 @@ end
 
 get "/latest_stats" do
   binding.pry
-  dataset = Dataset.first(:name => Setting.for("default_dataset_name"), :order => :created_at.desc, :summary_id.ne => nil)
-  summary = dataset.summary
+  summary = nil
+  offset = 0
+  while summary.nil?
+    dataset = Dataset.first(:name => Setting.for("default_dataset_name"), :order => :created_at.desc, :summary_id.ne => nil, :offset => offset)
+    summary = dataset.summary
+    offset+=1
+    break if offset >= Dataset.count(:name => Setting.for("default_dataset_name"), :summary_id.ne => nil)
+  end
   @results = Hashie::Mash[summary.results]
   @results.finished_at = dataset.updated_at
   erb :"summary/latest", :layout => :"layouts/public"
