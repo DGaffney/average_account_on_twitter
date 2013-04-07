54a52883f5f204727357288663aeb2167711f414
Devin Gaffney <itsme@devingaffney.com>
2013-04-06 22:54:18 -0700

    woo

--- handlers/compare.rb
+++ handlers/compare.rb
@@ -1,6 +1,6 @@
 get "/compare/:screen_name" do
   @results = {}
   @results[:user] = current_user.twitter_data
-  @results[:latest_summary] = Hashie::Mash[Summary.first(:order => :created_at.asc).results]
+  @results[:latest_summary] = Hashie::Mash[Summary.first(:order => :created_at.desc).results]
   erb :compare, :layout => :"layouts/public"
 end
--- handlers/summary.rb
+++ handlers/summary.rb
@@ -1,2 +1,6 @@
 get "/summary/latest.json" do
 end
+
+get "/latest_stats" do
+  @results = Hashie::Mash[Summary.first(:order => :created_at.desc).results]
+end
