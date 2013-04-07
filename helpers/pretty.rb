90f691864083bd375e54af932b8c739b724c4a08
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 16:20:32 -0700

    woo

--- helpers/pretty.rb
+++ helpers/pretty.rb
@@ -1,6 +1,6 @@
 module PrettyHelper
   def language_map
-    {"en" => "English", "ja" => "Japanese", "it" => "Italian", "de" => "German", "fr" => "French", "kr" => "Korean", "es" => "Spanish", "id" => "Indonesian", "fil" => "Filipino", "nl" => "Dutch", "pt" => "Portuguese", "pl" => "Polish", "sv" => "Swedish", "ru" => "Russian", "ar" => "Arabic", "no" => "Norwegian", "tr" => "Turkish", "hu" => "Hungarian", "zh-cn" => "Mandarin", "ko" => "Korean"}
+    {"fr"=>"French", "en"=>"English", "ar"=>"Arabic", "ja"=>"Japanese", "es"=>"Spanish", "de"=>"German", "it"=>"Italian", "id"=>"Indonesian", "pt"=>"Portuguese", "ko"=>"Korean", "tr"=>"Turkish", "ru"=>"Russian", "nl"=>"Dutch", "fil"=>"Filipino", "msa"=>"Malay", "zh-tw"=>"Traditional Chinese", "zh-cn"=>"Simplified Chinese", "hi"=>"Hindi", "no"=>"Norwegian", "sv"=>"Swedish", "fi"=>"Finnish", "da"=>"Danish", "pl"=>"Polish", "hu"=>"Hungarian", "fa"=>"Farsi", "he"=>"Hebrew", "ur"=>"Urdu", "th"=>"Thai"}
   end
   
   def language_name(lang)
