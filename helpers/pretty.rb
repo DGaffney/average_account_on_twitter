8f52165da13e6cb99e6ca45f1bb88fdce554275b
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 16:26:12 -0700

    woo

--- helpers/pretty.rb
+++ helpers/pretty.rb
@@ -1,6 +1,6 @@
 module PrettyHelper
   def language_map
-    {"fr"=>"French", "en"=>"English", "ar"=>"Arabic", "ja"=>"Japanese", "es"=>"Spanish", "de"=>"German", "it"=>"Italian", "id"=>"Indonesian", "pt"=>"Portuguese", "ko"=>"Korean", "tr"=>"Turkish", "ru"=>"Russian", "nl"=>"Dutch", "fil"=>"Filipino", "msa"=>"Malay", "zh-tw"=>"Traditional Chinese", "zh-cn"=>"Simplified Chinese", "hi"=>"Hindi", "no"=>"Norwegian", "sv"=>"Swedish", "fi"=>"Finnish", "da"=>"Danish", "pl"=>"Polish", "hu"=>"Hungarian", "fa"=>"Farsi", "he"=>"Hebrew", "ur"=>"Urdu", "th"=>"Thai"}
+    {"fr"=>"French", "en"=>"English", "ar"=>"Arabic", "ja"=>"Japanese", "es"=>"Spanish", "de"=>"German", "it"=>"Italian", "id"=>"Indonesian", "pt"=>"Portuguese", "ko"=>"Korean", "tr"=>"Turkish", "ru"=>"Russian", "nl"=>"Dutch", "fil"=>"Filipino", "msa"=>"Malay", "zh-tw"=>"Traditional Chinese", "zh-cn"=>"Simplified Chinese", "hi"=>"Hindi", "no"=>"Norwegian", "sv"=>"Swedish", "fi"=>"Finnish", "da"=>"Danish", "pl"=>"Polish", "hu"=>"Hungarian", "fa"=>"Farsi", "he"=>"Hebrew", "ur"=>"Urdu", "th"=>"Thai", "cs" => "Czech", "ca" => "Catalan", "el" => "Greek"}
   end
   
   def language_name(lang)
