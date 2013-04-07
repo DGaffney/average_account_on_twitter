c264460bf8969c3b29e99f74ca788d763d3f993d
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 13:34:48 -0700

    woo

--- handlers/summary.rb
+++ handlers/summary.rb
@@ -2,5 +2,7 @@ get "/summary/latest.json" do
 end
 
 get "/latest_stats" do
+  binding.pry
   @results = Hashie::Mash[Summary.first(:order => :created_at.desc).results]
+  erb :"summary/latest", :layout => :"layouts/public"
 end
