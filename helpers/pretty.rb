e06bca8ce918669e4f37ecc592f0fb9531d9858b
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 19:23:43 -0700

    woo

--- helpers/pretty.rb
+++ helpers/pretty.rb
@@ -6,4 +6,15 @@ module PrettyHelper
   def language_name(lang)
     language_map[lang]
   end
+  
+  def name_for_length_longitudinal(length)
+    return {
+      "day" => "Day",
+      "week" => "for the past week of data",
+      "month" => "for the past month of data",
+      "6month" => "for the past six months of data",
+      "year" => "for the past year of data",
+      "all_time" => "for all time recorded",
+    }[length]
+  end
 end
