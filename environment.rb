a7d675c270302c7397d993423e2da7bfc57a1cb0
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 22:34:23 -0700

    woo

--- environment.rb
+++ environment.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'pry'
 Dir[File.dirname(__FILE__) + '/model/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/extensions/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/before_hooks/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/helpers/*.rb'].each {|file| require file }
 MongoMapper.connection = Mongo::Connection.new("127.0.0.1", 27017, :pool_size => 60, :pool_timeout => 60)
