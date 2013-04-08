71ee15307cf2e6efde376b850c6eea2f1c6f4334
Devin Gaffney <itsme@devingaffney.com>
2013-04-08 00:08:24 -0700

    woo

--- handlers/post.rb
+++ handlers/post.rb
@@ -1,15 +1,15 @@
-get "/posts" do
-  erb :"posts/index", :layout => :"layouts/public"
-end
-
 get "/posts.json" do
   return @posts.to_json
 end
 
-get "/posts/:slug" do
-  erb :"posts/show", :layout => :"layouts/public"
+get "/posts" do
+  erb :"posts/index", :layout => :"layouts/public"
 end
 
 get "/posts/:slug.json" do
   return @post.to_json
+end
+
+get "/posts/:slug" do
+  erb :"posts/show", :layout => :"layouts/public"
 end
