bc184ce4fbaa04bd8ee8ba27f57aaa17bd7aa230
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 21:26:34 -0700

    woo

--- handlers/summary.rb
+++ handlers/summary.rb
@@ -50,15 +50,33 @@ get "/longitudinal" do
   @results = {}
   case (params[:length]||="all_time")
   when "day"
-    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60).collect{|d| @results[d.created_at] = d.summary.results if d.summary}
   when "week"
-    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7).collect{|d| @results[d.created_at] = d.summary.results if d.summary}
   when "month"
-    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*4).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*4).collect{|d| @results[d.created_at] = d.summary.results if d.summary}
   when "year"
-    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*365).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*365).collect{|d| @results[d.created_at] = d.summary.results if d.summary}
   when "all_time"
     Dataset.all(:order => :created_at.desc).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  end
+  summary_data = {:estimated_population => [], :statuses_count => [], :friends_count => [], :followers_count => [], :favourites_count => [], :listed_count => [], :created_at => [], :default_profile => [], :invalid_accounts => [], :total => []}  
+  @results.values.select{|v| !v.empty?}.each do |result_set|
+    [:statuses_count, :friends_count, :followers_count, :favourites_count, :listed_count, :created_at].each do |key|
+      if key == :created_at
+        summary_data[key] << result_set[key.to_s]["mean"].to_i if result_set[key.to_s]
+      else
+        summary_data[key] << result_set[key.to_s]["mean"] if result_set[key.to_s]
+      end
+    end
+    summary_data[:estimated_population] << result_set["estimated_population"]
+    summary_data[:default_profile] << result_set["default_profile"]["true"] if result_set["default_profile"]
+    summary_data[:invalid_accounts] << result_set["invalid_accounts"]
+    summary_data[:total] << result_set["total"]
+  end
+  @executive_summary = {}
+  summary_data.each_pair do |attribute, means|
+    @executive_summary[attribute] = means.all_stats
   end
   erb :"summary/longitudinal", :layout => :"layouts/public"
 end
