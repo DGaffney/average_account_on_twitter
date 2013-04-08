37e9097c3f624ca6bd49a48d6dc1f2ecf0a1811b
Devin Gaffney <itsme@devingaffney.com>
2013-04-07 19:18:52 -0700

    woo

--- handlers/summary.rb
+++ handlers/summary.rb
@@ -45,3 +45,20 @@ get "/stats/page/:page" do
   })
   erb :"summary/index", :layout => :"layouts/public"
 end
+
+get "/longitudinal" do
+  @results = {}
+  case (params[:length]||="all_time")
+  when "day"
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  when "week"
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  when "month"
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*4).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  when "year"
+    Dataset.all(:order => :created_at.desc, :created_at.gte => Time.now-24*60*60*7*365).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  when "all_time"
+    Dataset.all(:order => :created_at.desc).collect{|d| @results[d.created_at] = d.summary.nil? ? {} : d.summary.results}
+  end
+  erb :"summary/longitudinal", :layout => :"layouts/public"
+end
