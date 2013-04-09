1ab878c8caa999a9c85ddee9e68bfe79403411b9
Devin Gaffney <itsme@devingaffney.com>
2013-04-08 17:19:32 -0700

    woo

--- before_hooks/compare.rb
+++ before_hooks/compare.rb
@@ -1,5 +1,13 @@
 before "/compare/:screen_name*" do
   @results = {}
+  summary = nil
+  offset = 0
+  while summary.nil?
+    dataset = Dataset.first(:name => Setting.for("default_dataset_name"), :order => :created_at.desc, :summary_id.ne => nil, :offset => offset)
+    summary = dataset.summary
+    offset+=1
+    break if offset >= Dataset.count(:name => Setting.for("default_dataset_name"), :summary_id.ne => nil)
+  end
   @results[:user] = current_user.twitter_data_for(params[:screen_name].split(".").first)
-  @results[:latest_summary] = Hashie::Mash[Summary.first(:order => :created_at.desc).results]
+  @results[:latest_summary] = Hashie::Mash[summary.results]
 end
